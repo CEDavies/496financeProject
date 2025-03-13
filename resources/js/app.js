9b988b1ae025f0d1e6610f700b96106df5305d81
ajbeas2015 <130802986+ajbeas2015@users.noreply.github.com>
2025-03-13 13:21:15 -0500

    update to database

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,10 @@
 import './bootstrap';
-import Counter from './components/Counter.vue'
 import Welcome from './components/welcome.vue';
 import { createApp } from 'vue'
 
 const app = createApp()
 
-app.component('counter', Counter)
+
 app.component('welcome', Welcome)
 
 app.mount('#app')
