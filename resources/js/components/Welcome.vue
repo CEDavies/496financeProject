5a029a5adacf8dd8f02bdcd0a4e2e86db78649ed
ajbeas2015 <130802986+ajbeas2015@users.noreply.github.com>
2025-04-03 13:19:23 -0500

    Update Welcome.vue

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -20,11 +20,11 @@ import VueRouter from 'vue-router';
       <h2 class="text-xl font-semibold mb-4">Welcome, please login or sign up!</h2>
 
       <div class="space-x-20 px-10">
-        <router-link to="/Login">
+        <router-link to="/Login.vue">
           <button type="button" class="px-6 py-2 bg-blue-500 text-white rounded-md">Login</button>
         </router-link>
 
-        <RouterLink to="/Signup">
+        <RouterLink to="/Signup.vue">
           <button type="button" class="px-6 py-2 bg-green-500 text-white rounded-md">Sign Up</button>
         </RouterLink>
       </div>
