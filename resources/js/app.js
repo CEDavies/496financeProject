5da0f190e151cd1f9a9cfeaf7e71b6b539cd5322
CEDavies <141366203+CEDavies@users.noreply.github.com>
2025-04-22 21:46:43 -0500

    dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
-import './bootstrap';
-import Welcome from './components/Welcome.vue';
-import { createApp } from 'vue';
-
-const app = createApp();
-
-app.component('welcome', Welcome);
-
-app.mount('#app');
 
 import { createApp } from 'vue'
 import App from './App.vue'
---
+++ resources/js/views/OauthSuccess.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>OAuth Login Successful!</h1>
+      <p>You’ve been logged in with Google.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'OauthSuccess'
+  }
+  </script>
+  
