f33a8d507838bf289d1528301c298e4d2e0829c9
ajbeas2015 <130802986+ajbeas2015@users.noreply.github.com>
2025-04-27 00:25:28 -0500

    Revert "Update InvestmentOpt.vue"
    This reverts commit 6293428fb2c481f23413963b584d462f06477547.

--- resources/js/components/teacher/InvestmentOpt.vue
+++ resources/js/components/teacher/InvestmentOpt.vue
@@ -52,11 +52,12 @@ const { homeRoute, manageStud, manageInvest, projectRoute, reportRoute } = defin
           </thead>
           <tbody>
             <tr>
-              <td class="border px-4 py-2">Name</td>
-              <td class="border px-4 py-2">Duration</td>
-              <td class="border px-4 py-2">Interest Type</td>
-              <td class="border px-4 py-2">Interest Rate</td>
+              <td class="border px-4 py-2">{{investment.name}}</td>
+              <td class="border px-4 py-2">{{investment.duration_year}}</td>
+              <td class="border px-4 py-2">{{investment.interest.type}}</td>
+              <td class="border px-4 py-2">{{investment.interest_rate}}</td>
             </tr>
+            <!--
             <tr>
               <td class="border px-4 py-2">Name</td>
               <td class="border px-4 py-2">Duration</td>
@@ -69,6 +70,7 @@ const { homeRoute, manageStud, manageInvest, projectRoute, reportRoute } = defin
               <td class="border px-4 py-2">Interest Type</td>
               <td class="border px-4 py-2">Interest Rate</td>
             </tr>
+            !-->
           </tbody>
         </table>
 
