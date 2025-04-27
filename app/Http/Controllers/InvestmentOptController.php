7ec8178c6483f50ce9324894d28eba1089425a47
ajbeas2015 <130802986+ajbeas2015@users.noreply.github.com>
2025-04-27 00:27:13 -0500

    rename fix

--- app/Http/Controllers/InvestmentOptController.php
+++ app/Http/Controllers/InvestmentOptController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class InvestmnetOptController extends Controller
+class InvestmentOptController extends Controller
 {
     public function extractInvestment(Request $request)
     {
