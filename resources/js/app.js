b9f0d120a774e5cb490fe8b18d5c2a15746db250
CEDavies <141366203+CEDavies@users.noreply.github.com>
2025-05-01 12:11:07 -0500

    Revert "Green apple"

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 import './bootstrap';
+import Welcome from './components/Welcome.vue';
 import { createApp } from 'vue';
-import App from './App.vue'; // Root component
-import router from './router';
 
-const app = createApp(App);
-app.use(router);
+const app = createApp();
+
+app.component('welcome', Welcome);
+
 app.mount('#app');
+
+import App from './App.vue'
+//import router from './router/index.js'
+
+
+createApp(App).use(router).mount('#app')
